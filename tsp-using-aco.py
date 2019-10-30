a4c2c739014667e0ad7195a3055eed00bd7d6b9f
upadhyayatul <42071889+upadhyayatul@users.noreply.github.com>
2019-10-30 18:48:39 +0530

    Update tsp-using-aco.py

--- tsp-using-aco.py
+++ tsp-using-aco.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 cities = np.array(['A','B','C','D','E'])
 n = np.random.choice(cities)
 
+#---------------------------------distance calculation of every node----------------------------
 x = []
 y = []
 
@@ -16,9 +17,6 @@ for i in range(5):
     
     plt.scatter(x_axis,y_axis,s = 40)
     plt.text(x_axis,y_axis,cities[i],fontsize = 12)
-    #plt.annotate(cities,plt.scatter(x_axis,y_axis,s = 1
-#plt.xlim(0,10)
-#plt.ylim(0,10)
 print(x)
 print(y)
 
@@ -33,14 +31,50 @@ print()
 distance = []
 for i in range(0,5):
     for j in range(0,5):
-        #if((i) == (j)):
-            #continue
-        #else:
         distance.append((((x[j]-x[i])**2 + (y[j] - y[i])**2)**0.5))
 distance = np.array(distance)
 distance = np.reshape(distance,(5,5))
-distance
-#print(count)
-#for j in range(1,len(cities)):
-#        b.append((((x[j]-x[0])**2 + (y[j] - y[0])**2)**0.5))
-#print(b)
+print(distance)
+
+#----------------value initialisation-------------------------
+pheromene = np.ones((5,5))
+sum_pheromene = np.zeros((5,5))
+epoch = 100
+alpha = 1
+beta = 2
+
+eta = np.zeros((5,5))
+rho = 0.2
+cumulative_lst = []
+
+a = [x[0]]
+b = [y[0]]
+
+axis_list = [0]
+number_of_ants = 10
+
+#-----------------------path i,j probability--------------------
+for i in range(0,5):
+        for j in range(0,5):
+            if(distance[i][j] == 0):
+                eta[i][j] = 0
+            else:
+                eta[i][j] = (1/distance[i][j]) 
+
+    sum_pheromene_eta =  np.sum((((pheromene)**alpha)*((eta)**beta)))
+
+def probability(alpha,beta,i,j):
+        prob = (((pheromene[i][j])**alpha)*((eta[i][j])**beta))/(sum_pheromene_eta)
+        return prob
+    
+prob = []
+    for i in range(0,5):
+        for j in range(0,5):
+            temp = 0
+            temp = probability(1,2,i,j)
+            prob.append(temp)
+
+    prob = np.array(prob)
+    prob = prob.reshape(5,5)
+    print(prob)
+    
